5e9b1a185aa0631df9761fdda4fda8a57f981dff
Christopher Wellons <wellons@nullprogram.com>
2014-12-06 11:30:05 -0500

    Add enemy ships.

--- dosdef.c
+++ dosdef.c
@@ -13,16 +13,17 @@
 #define PARTICLE_MAX_AGE   50
 
 typedef unsigned int tick_t;
+typedef void (*ai_t)(int id);
 
 static tick_t ticks;
 
 struct ship {
     int32_t x, y, dx, dy;
     tick_t last_fire;
+    ai_t ai;
     uint8_t fire_delay;
     uint8_t color_a, color_b;
     uint8_t hp, hp_max;
-    bool alive;
 };
 
 struct bullet {
@@ -39,10 +40,10 @@ struct particle {
 };
 
 static struct bullet *bullets;
-static size_t bullets_max = 128;
+static size_t bullets_max = 64;
 
 static struct particle *particles;
-static size_t particles_max = 128;
+static size_t particles_max = 64;
 
 static struct ship *ships;
 static size_t ships_max = 16;
@@ -62,11 +63,11 @@ static void bullet_step(int i)
         bullets[i].alive = false;
 }
 
-static int ship_fire(struct ship *source)
+static int ship_fire(int i)
 {
-    if (source->last_fire + source->fire_delay > ticks)
+    if (ships[i].last_fire + ships[i].fire_delay > ticks)
         return -1;
-    source->last_fire = ticks;
+    ships[i].last_fire = ticks;
     int choice = 0;
     for (int i = 0; i < bullets_max; i++) {
         if (!bullets[i].alive) {
@@ -78,11 +79,11 @@ static int ship_fire(struct ship *source)
     }
     if (bullets[choice].alive)
         bullet_draw(choice, true);
-    bullets[choice].x = source->x + source->dx / 100;
-    bullets[choice].y = source->y + source->dy / 100;
-    bullets[choice].dx = source->dx * BULLET_SPEED;
-    bullets[choice].dy = source->dy * BULLET_SPEED;
-    bullets[choice].color = source->color_b;
+    bullets[choice].x = ships[i].x + ships[i].dx / 100;
+    bullets[choice].y = ships[i].y + ships[i].dy / 100;
+    bullets[choice].dx = ships[i].dx * BULLET_SPEED;
+    bullets[choice].dy = ships[i].dy * BULLET_SPEED;
+    bullets[choice].color = ships[i].color_b;
     bullets[choice].birthtick = ticks;
     bullets[choice].alive = true;
     return choice;
@@ -132,32 +133,33 @@ static void burn(int32_t x, int32_t y)
     particles[choice].birthtick = ticks;
 }
 
-static void ship_draw(struct ship *ship, bool clear)
+static void ship_draw(int id, bool clear)
 {
-    struct point c = {ship->x / SCALE, ship->y / SCALE};
+    struct point c = {ships[id].x / SCALE, ships[id].y / SCALE};
     for (int i = -1; i <= 1; i++) {
-        struct point ha = {ship->x / SCALE - i, ship->y / SCALE - 2};
-        struct point hb = {ship->x / SCALE - i, ship->y / SCALE + 2};
-        struct point va = {ship->x / SCALE - 2, ship->y / SCALE - i};
-        struct point vb = {ship->x / SCALE + 2, ship->y / SCALE - i};
-        vga_pixel(ha, clear ? BACKGROUND : ship->color_a);
-        vga_pixel(va, clear ? BACKGROUND : ship->color_a);
-        vga_pixel(hb, clear ? BACKGROUND : ship->color_a);
-        vga_pixel(vb, clear ? BACKGROUND : ship->color_a);
+        struct point ha = {ships[id].x / SCALE - i, ships[id].y / SCALE - 2};
+        struct point hb = {ships[id].x / SCALE - i, ships[id].y / SCALE + 2};
+        struct point va = {ships[id].x / SCALE - 2, ships[id].y / SCALE - i};
+        struct point vb = {ships[id].x / SCALE + 2, ships[id].y / SCALE - i};
+        vga_pixel(ha, clear ? BACKGROUND : ships[id].color_a);
+        vga_pixel(va, clear ? BACKGROUND : ships[id].color_a);
+        vga_pixel(hb, clear ? BACKGROUND : ships[id].color_a);
+        vga_pixel(vb, clear ? BACKGROUND : ships[id].color_a);
     }
-    struct point d = {c.x + ship->dx / 10, c.y + ship->dy / 10};
-    vga_pixel(d, clear ? BACKGROUND : WHITE);
+    struct point d = {c.x + ships[id].dx / 10, c.y + ships[id].dy / 10};
+    if (id == 0)
+        vga_pixel(d, clear ? BACKGROUND : WHITE);
 }
 
-static void ship_step(struct ship *ship)
+static void ship_step(int i)
 {
-    ship->x += ship->dx;
-    ship->y += ship->dy;
-    ship->dx = (ship->dx * 99) / 100;
-    ship->dy = (ship->dy * 99) / 100;
-    if (ship->hp < ship->hp_max / 2) {
-        if ((randn(ship->hp < 1 ? 1 : ship->hp) == 0))
-            burn(ship->x, ship->y);
+    ships[i].x += ships[i].dx;
+    ships[i].y += ships[i].dy;
+    ships[i].dx = (ships[i].dx * 99) / 100;
+    ships[i].dy = (ships[i].dy * 99) / 100;
+    if (ships[i].hp < ships[i].hp_max / 2) {
+        if ((randn(ships[i].hp < 1 ? 1 : ships[i].hp) == 0))
+            burn(ships[i].x, ships[i].y);
     }
 }
 
@@ -173,15 +175,64 @@ static void print_game_over()
     vga_print((struct point){133, 97}, WHITE, "GAME OVER");
 }
 
-static void ship_check_bounds(struct ship *ship)
+static void ship_check_bounds(int i)
 {
     int32_t xlim = VGA_PWIDTH * SCALE;
     int32_t ylim = VGA_PHEIGHT * SCALE;
-    if (ship->x < 0 || ship->x > xlim || ship->y < 0 || ship->y > ylim) {
-        ship->dx = 0;
-        ship->dy = 0;
-        ship->hp = 0;
+    if (ships[i].x < 0 || ships[i].x > xlim ||
+        ships[i].y < 0 || ships[i].y > ylim) {
+        ships[i].dx = 0;
+        ships[i].dy = 0;
+        ships[i].hp = 0;
     }
+}
+
+int spawn(int hp, ai_t ai)
+{
+    int choice = -1;
+    for (int i = 1; i < ships_max; i++) {
+        if (!ships[i].hp > 0) {
+            choice = i;
+            break;
+        }
+    }
+    if (choice > -1) {
+        ships[choice].hp = hp;
+        ships[choice].dx = ships[choice].dy = 0;
+        if (randn(2)) {
+            ships[choice].x = randn(2) * VGA_PWIDTH * SCALE;
+            ships[choice].y = randn(VGA_PHEIGHT * SCALE);
+        } else {
+            ships[choice].x = randn(VGA_PWIDTH * SCALE);
+            ships[choice].y = randn(2) * VGA_PHEIGHT * SCALE;
+        }
+        ships[choice].ai = ai;
+    }
+    return choice;
+}
+
+static void ai_player(int i)
+{
+    int xrange = 2 * (joystick_config[0].xmax - joystick_config[0].xmin);
+    int yrange = 2 * (joystick_config[0].ymax - joystick_config[0].ymin);
+    if (ships[i].hp > 0) {
+        struct joystick joy;
+        joystick_read(&joy);
+        ships[i].dx += ((joy.x - joystick_config[0].xcenter) * 100) / xrange;
+        ships[i].dy += ((joy.y - joystick_config[0].xcenter) * 100) / yrange;
+        if (joy.a)
+            ship_fire(i);
+    }
+}
+
+static void ai_seeker(int i)
+{
+    int noise = 400;
+    int dx = ships[0].x - ships[i].x;
+    int dy = ships[0].y - ships[i].y;
+    ships[i].dx = dx / 250 + randn(noise) - noise / 2;
+    ships[i].dy = dy / 250 + randn(noise) - noise / 2;
+    ship_fire(i);
 }
 
 int _main(void)
@@ -204,9 +255,9 @@ int _main(void)
     joystick_config[0].xmin = joystick_config[0].ymin = 1;
     joystick_config[0].xcenter = joystick_config[0].ycenter = 128;
     //joystick_calibrate();
-    struct joystick_config jconf = joystick_config[0];
-    struct ship *player = &ships[0];
-    *player = (struct ship){
+
+    /* Initialize Ships */
+    ships[0] = (struct ship) {
         .x = VGA_PWIDTH / 2 * SCALE,
         .y = VGA_PHEIGHT / 2 * SCALE,
         .color_a = YELLOW,
@@ -214,40 +265,41 @@ int _main(void)
         .fire_delay = 10,
         .hp = 100,
         .hp_max = 100,
+        .ai = ai_player
     };
-    int xrange = 2 * (jconf.xmax - jconf.xmin);
-    int yrange = 2 * (jconf.ymax - jconf.ymin);
 
     /* Main Loop */
     vga_clear(BACKGROUND);
     for (;;) {
-        ship_draw(player, true);
-        struct joystick joy;
-        if (player->hp > 0) {
-            joystick_read(&joy);
-            player->dx += ((joy.x - jconf.xcenter) * 100) / xrange;
-            player->dy += ((joy.y - jconf.xcenter) * 100) / yrange;
-        } else {
+        if (randn(100) == 0) {
+            int id = spawn(2, ai_seeker);
+            if (id >= 0) {
+                ships[id].color_a = BROWN;
+                ships[id].color_b = LIGHT_RED;
+                ships[id].fire_delay = 100;
+                ships[id].hp_max = 2;
+            }
+        }
+
+        if (ships[0].hp == 0)
             print_game_over();
-            joy.x = jconf.xcenter;
-            joy.y = jconf.ycenter;
-            joy.a = false;
-            joy.b = false;
+        for (int i = 0; i < ships_max; i++) {
+            if (ships[i].hp > 0 || i == 0) {
+                ship_draw(i, true);
+                ship_step(i);
+                ships[i].ai(i);
+                if (ships[i].hp > 0)
+                    ship_draw(i, false);
+            }
         }
-        ship_step(player);
-        ship_draw(player, false);
-        ship_check_bounds(player);
+        ship_check_bounds(0);
         /* TODO: temporary testing button */
-        if (joy.b) {
-            player->hp--;
-        }
-        if (joy.a)
-            ship_fire(player);
         for (int i = 0; i < bullets_max; i++) {
             bullet_draw(i, true);
             if (bullets[i].alive) {
                 bullet_step(i);
-                bullet_draw(i, false);
+                if (bullets[i].alive)
+                    bullet_draw(i, false);
             }
         }
         for (int i = 0; i < particles_max; i++) {
